c5743eaa8b3eb469ec61b4e5f0605231ba2cf4e4
Stefan Bochinger <stefanbochinger@gmail.com>
2025-11-26 00:08:04 +0100

    Unity opens server connection

---
+++ UnityGameFiles/Assets/Scripts/Model/PythonServerManager.cs
@@ -0,0 +1,29 @@
+using System.Diagnostics;
+using System.IO;
+using UnityEngine;
+
+public class PythonServerManager
+{
+    private Process pythonProcess;
+
+    public void Start()
+    {
+        string pythonExe = Path.Combine(Application.dataPath, "Python/win/python-3.13.9-embed-amd64/python.exe");
+        string scriptPath = Path.Combine(Application.dataPath, "Python/shared/TinyCLIP.py");
+
+        pythonProcess = new Process();
+        pythonProcess.StartInfo.FileName = pythonExe;
+        pythonProcess.StartInfo.Arguments = scriptPath;
+        pythonProcess.StartInfo.UseShellExecute = true;
+        pythonProcess.StartInfo.CreateNoWindow = true;
+        pythonProcess.Start();
+
+        UnityEngine.Debug.Log("Python server started.");
+    }
+
+    public void OnApplicationQuit()
+    {
+        if (!pythonProcess.HasExited)
+            pythonProcess.Kill();
+    }
+}
